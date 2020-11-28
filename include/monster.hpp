38bba4d27016b98a6ac64c9b59ff441226b49e0f
Grace <complex.invoke@gmail.com>
2020-11-28 22:18:00 +0800

    permutation_index

--- include/monster.hpp
+++ include/monster.hpp
@@ -20,7 +20,7 @@
  *   time a set of code changes is merged to the master branch.
  */
 
-#define MONSTER_VERSION 208
+#define MONSTER_VERSION 209
 
 #define MONSTER_VERSION_STRING "Monster/" STRINGIZE(MONSTER_VERSION)
 
