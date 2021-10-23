d58614c59b7f858b0278daa004ecf1bf1a472e4b
Grace <complex.invoke@gmail.com>
2021-10-23 19:42:32 +0800

    unique_tuple

---
+++ example/unique_tuple.cpp
@@ -0,0 +1,42 @@
+//
+// Copyright (c) 2016-present DeepGrace (complex dot invoke at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/deepgrace/monster
+//
+
+// g++ -I include -m64 -std=c++20 -s -Wall -Os -o /tmp/unique_tuple example/unique_tuple.cpp
+
+#include <iostream>
+#include <unique_tuple.hpp>
+
+using namespace monster;
+
+constexpr bool operator==(auto lhs, auto rhs)
+{
+    return lhs.value == rhs.value;
+}
+
+template <typename T>
+struct X
+{
+    T value;
+};
+
+int main(int argc, char* argv[])
+{
+    constexpr std::tuple t(X(7), X(7), X(4), X(3), X(3), X(4), X(6));
+
+    constexpr auto v = unique_tuple(t);
+    constexpr auto p = std::get<v.index()>(v);
+
+    std::apply([]<typename... Args>(Args&&... args)
+    {
+        ((std::cout << args.value << " "), ...);
+        std::cout << std::endl;
+    }, p);
+
+    return 0;
+}
---
+++ include/unique_tuple.hpp
@@ -0,0 +1,65 @@
+//
+// Copyright (c) 2016-present DeepGrace (complex dot invoke at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/deepgrace/monster
+//
+
+#ifndef UNIQUE_TUPLE_HPP
+#define UNIQUE_TUPLE_HPP
+
+#include <tuple>
+#include <variant>
+#include <algorithm>
+
+namespace monster
+{
+    template <size_t N>
+    using index_t = std::integral_constant<size_t, N>;
+
+    template <typename T, auto N>
+    constexpr decltype(auto) to_tuple(const std::array<T, N>& a, auto l)
+    {
+        return [&]<auto... n>(std::index_sequence<n...>)
+        {
+            return std::tuple(a[n]...);
+        }
+        (std::make_index_sequence<l + 1>());
+    }
+
+    template <typename T, auto M, auto... N>
+    constexpr std::variant<decltype(to_tuple(std::array<T, M>(), index_t<N>()))...>
+    to_variant(const std::array<T, M>& a, auto l, std::index_sequence<N...>)
+    {
+        return to_tuple(a, l);
+    }
+
+    template <typename T>
+    constexpr decltype(auto) unique_tuple(const T& t)
+    {
+        constexpr auto N = std::tuple_size_v<T>;
+
+        auto a = std::apply([]<typename... Args>(Args&&... args)
+        {
+            return std::array<std::common_type_t<Args...>, N>{std::forward<Args>(args)...};
+        }, t);
+
+        using indices = std::make_index_sequence<N>;
+
+        constexpr auto index = []<auto... n>(std::index_sequence<n...>)
+        {
+            using variant_t = std::variant<index_t<n>...>;
+            std::array index {variant_t{std::in_place_index<n>}...};
+
+            return index;
+        }
+        (indices());
+
+        auto k = std::unique(a.begin(), a.end()) - a.begin();
+        return std::visit([&](auto l) { return to_variant(a, l, indices()); }, index[k - 1]);
+    }
+}
+
+#endif
